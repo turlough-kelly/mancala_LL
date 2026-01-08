e5315f8553dccca89206229b9f0524266401d835
turlough-kelly <72017650+turlough-kelly@users.noreply.github.com>
2026-01-08 21:00:45 +0000

    Added tests to implement in future

--- src/test/java/kelly/turlough/test/testSuite.java
+++ src/test/java/kelly/turlough/test/testSuite.java
@@ -14,4 +14,33 @@ public class testSuite
         int bingus = 1;
         Assert.assertEquals(1, bingus);
     }
+
+    @Test
+    public void checkCapture()
+    {
+        int bingus = 1;
+        Assert.assertEquals(1, bingus);
+    }
+
+
+    @Test
+    public void checkCompleteBoardTraversal()
+    {
+        int bingus = 1;
+        Assert.assertEquals(1, bingus);
+    }
+
+    @Test
+    public void checkEndGameScenario()
+    {
+        int bingus = 1;
+        Assert.assertEquals(1, bingus);
+    }
+
+    @Test
+    public void checkOutOfBoundsExceptions()
+    {
+        int bingus = 1;
+        Assert.assertEquals(1, bingus);
+    }
 }
