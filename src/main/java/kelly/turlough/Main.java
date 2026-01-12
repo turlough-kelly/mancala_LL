24bc3cff0972fbdfc349041164d664a6282d4006
turlough-kelly <72017650+turlough-kelly@users.noreply.github.com>
2026-01-12 21:45:04 +0000

    small test for second player turn

--- src/main/java/kelly/turlough/Main.java
+++ src/main/java/kelly/turlough/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args)
     {
         Mancala newGame = new Mancala();
-        newGame.turn(1);
+        newGame.turn(2);
         newGame.board.printList();
 
 
